d72e5127ca5b044bd8c8a6fda1d8c2c09abe97fb
HaiderAli99888 <122993240+HaiderAli99888@users.noreply.github.com>
2024-02-11 21:29:45 -0500

    Assignemnt1 completed

--- H_Ali_MyFavouritePolitician/src/app/content-card/content-card.component.ts
+++ H_Ali_MyFavouritePolitician/src/app/content-card/content-card.component.ts
@@ -16,7 +16,7 @@ export class ContentCardComponent {
       description: 'Imran Ahmad Khan Niazi born 5 October 1952 is a Pakistani politician and former cricketer who served as the 22nd prime minister of Pakistan from August 2018 until April 2022.',
       creator: 'Haider Ali',
       imgURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Imran_Ahmed_Khan_Niazi_-_UNGA_%2848784380531%29_%28cropped%29.jpg/220px-Imran_Ahmed_Khan_Niazi_-_UNGA_%2848784380531%29_%28cropped%29.jpg',
-      type: 'Politicianms',
+      type: 'Politicians',
       tags: ['politician', 'pakistan']
     });
 
@@ -36,7 +36,7 @@ export class ContentCardComponent {
       description: 'Narendra Damodardas Modi is an Indian politician who has served as the 14th prime minister of India since May 2014.',
       creator: 'Haider Ali',
       imgURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Official_Photograph_of_Prime_Minister_Narendra_Modi_Portrait.png/220px-Official_Photograph_of_Prime_Minister_Narendra_Modi_Portrait.png',
-      type: 'Cities',
+      type: 'Politicians',
       tags: ['politician', 'india']
     });
   }
